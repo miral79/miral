82ddd17fd3a40ac1e881338aa49966daca145008
miral butani <miralbutani792002@gmail.com>
2024-07-04 17:14:06 +0530

    miral

--- lib/page/Splash_screen.dart
+++ lib/page/Splash_screen.dart
@@ -1,6 +1,8 @@
+// ignore: file_names
 import 'package:flutter/material.dart';
 import 'package:social_media/page/Login_screen.dart';
 
+// ignore: camel_case_types
 class splace_screen extends StatefulWidget {
   const splace_screen({super.key});
 
@@ -8,13 +10,14 @@ class splace_screen extends StatefulWidget {
   State<splace_screen> createState() => _splace_screen();
 }
 
+// ignore: camel_case_types
 class _splace_screen extends State<splace_screen> {
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    Future.delayed(Duration(milliseconds: 1), () {
+    Future.delayed(const Duration(milliseconds: 1), () {
       Navigator.push(
           context,
           MaterialPageRoute(
@@ -25,6 +28,6 @@ class _splace_screen extends State<splace_screen> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("miral_splace_screen"));
+    return const Center(child: Text("miral_splace_screen"));
   }
 }
